0487c6566632804f20cb54ed67091b4b7cf78398
lukeforte40 <lukeforte40@gmail.com>
2024-06-16 17:11:21 -0400

    added checkTail() to check a set of x,y coordinates against the current snakes tail locations.

--- src/Includes/checkPosition.cpp
+++ src/Includes/checkPosition.cpp
@@ -1,6 +1,8 @@
 // Includes
     #include "checkPosition.h"
     #include "globalVariables.h"
+    #include <algorithm>
+    #include <bits/stdc++.h>
 
 using namespace std;
 // Variables
@@ -11,15 +13,7 @@ using namespace std;
         {
             return true;
         }
-        int i = 0;
-        while (snakeTailX[i] != 0)
-        {
-            if (snakeTailX[i] == x && snakeTailY[i] == y)
-            {
-                return true;
-            }
-            i++;
-        }
+        checkTail(x, y);
         return false;
     }
     bool checkFruitPosition(int x, int y){
@@ -28,4 +22,38 @@ using namespace std;
             return true;
         }
         return false;
+    }
+    bool checkTail(int x, int y){
+        if (snakeTailX.empty())
+        {
+            return false;
+        }
+        // find index of first matching value
+        auto xIndex = find(snakeTailX.begin(),snakeTailX.end(), x);
+        switch (count(snakeTailX.begin(),snakeTailX.end(), x))
+        {
+        case 0:
+            return false;
+            break;
+        case 1:
+            // check if current x value has matching y variable
+            if (snakeTailY[xIndex - snakeTailX.begin()] == y)
+            {
+                return true;
+            }
+            return false;
+        default:
+            while (xIndex != snakeTailX.end())
+            {
+                // check if current x value has matching y variable
+                if (snakeTailY[xIndex - snakeTailX.begin()] == y)
+                {
+                    return true;
+                }
+                // find next value index
+                xIndex = find(xIndex + 1, snakeTailX.end(), x);
+            }            
+            return false;
+            break;
+        }
     }
--- src/Includes/checkPosition.h
+++ src/Includes/checkPosition.h
@@ -2,4 +2,5 @@
 #define checkPosition_h
     bool checkSnakePosition(int x, int y);
     bool checkFruitPosition(int x, int y);
+    bool checkTail(int x, int y);
 #endif // checkPosition_h
