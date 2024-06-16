32c849e4db91a94c7c09bce9902d5eb447e2d01d
lukeforte40 <lukeforte40@gmail.com>
2024-06-16 17:11:47 -0400

    re added global variables to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,28 @@
     #include "Includes/gameInit.h"
     #include "Includes/drawScreen.h"
     #include <iostream>
+    #include <vector>
 
 using namespace std;
+// Global variables
+    // Boundary sizing
+    const int gameHeight = 20;
+    const int gameWidth = 80;
+    // Snakes head coordinates
+    int snakeHeadX, snakeHeadY;
+    // Fruit coordinates
+    int fruitX, fruitY;
+    // Player score
+    int playerScore;
+    // Snake tail arrays
+    vector<int> snakeTailX, snakeTailY;
+    // Snake tail lenght
+    int tailLenght;
+    // Snakes direction variable
+    snakeDirection snakeDir;
+    // Check if the game is over or not
+    bool isGameOver;
+
 // Main function
     int main(){
         gameInit();
